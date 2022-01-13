d086466e2390bd64a96b82418acf6e9f02d3364d
esaave3 <esaave3@bancodebogota.com.co>
2022-01-13 18:02:04 -0500

    helpers

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,28 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
     const [images, setImages] = useState([])
     useEffect(() => {
-        getGifs();
-    }, []);
-
-    const getGifs = async () => {
-        const url = 'https://api.giphy.com/v1/gifs/search?api_key=AUs7p6rekqAqeAlbszWP11lCqeo6dYnL&q=Rick+and+Morty&limit=10';
-        const resp = await fetch(url);
-        const { data } = await resp.json();
-
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-
-            }
-        });
-        console.log(gifs);
-        setImages(gifs);
-    }
+        getGifs( category ).then(setImages);
+    }, [category]);
 
     return (
         <>
