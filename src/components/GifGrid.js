e1bcfdd6fc6ff3d0e29a028b7a913df6ddb850fe
esaave3 <esaave3@bancodebogota.com.co>
2022-02-03 17:49:15 -0500

    git expert

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([])
-    useEffect(() => {
-        getGifs( category ).then(setImages);
-    }, [category]);
-
+    const { data: images, loading } = useFetchGifs( category );
     return (
         <>
-            <h3> {category} </h3>
+            <h3 className='animate__animated animate__bounce'> {category} </h3>
+            { loading && <p className='animate__animated animate__flash'>Loading</p> }
             <div className='card-grid'>
 
                 {
